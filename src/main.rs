efba9913aea7e53ffd25d83bdcd80a7c7af7798c
Solomon Hawk <solomon.hawk@viget.com>
2023-09-12 01:06:57 -0400

    Render basic pixel sprite asset

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use bevy::prelude::*;
 
+fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("subjects/ruru.png"),
+        transform: Transform::from_xyz(100., 0., 0.),
+        ..default()
+    });
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(
+            // prevent low res texture blurring
+            ImagePlugin::default_nearest(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
 }
